1642dacdc21582f24e9ce74a36ad28a26a7dee18
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2022-12-28 21:59:34 +0000

    hero  section and navbar modification

--- Front-end/front_end/src/PrincipalPage/Components/Header.js
+++ Front-end/front_end/src/PrincipalPage/Components/Header.js
@@ -5,16 +5,17 @@ import { BsFacebook, BsTelephone } from "react-icons/bs";
 import { AiFillLinkedin, AiOutlineHome } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
+import Navbar from "./Navbar";
 
 const Header = () => (
-  <section>
+  <section className="h-[752px]">
     <div className="w-full h-[80px] flex flex-row items-center justify-between px-8">
       <div className="flex flex-row gap-2 max-w-[250px]">
         <AiOutlineMail size={25} />
         <h6>exemple@gmail.com</h6>
       </div>
-      <div className="flex flex-row items-center justify-center gap-1">
-        <div className="flex flex-row gap-3 cursor-pointer">
+      <div className="flex flex-row items-center justify-center gap-4 max-w-lg">
+        <div className="hidden md:flex md:flex-row gap-3 cursor-pointer">
           <BsFacebook size={20} />
           <AiFillLinkedin size={20} />
           <AiFillInstagram size={20} />
@@ -26,12 +27,19 @@ const Header = () => (
       </div>
     </div>
     <hr></hr>
-    <div className="h-[100px] bg-slate-400 mt-2"></div>
-    <div className="w-full flex flex-col items-center justify-center mt-[100px] gap-4 px-3">
-      <h1 className="text-3xl font-bold">Find your dream home</h1>
-      <p className="text-bold text-center">
+    <div className="w-full h-[60px] flex flex-row md:hidden items-center justify-center px-10 gap-8">
+      <BsFacebook size={30} />
+      <AiFillLinkedin size={30} />
+      <AiFillInstagram size={30} />
+    </div>
+    <Navbar />
+    <div className="w-full flex flex-col items-center justify-center mt-[170px] gap-4 px-3">
+      <h1 className="md:text-5xl text-3xl font-semibold text-slate-200">
+        Find your dream home
+      </h1>
+      <p className="text-bold text-center text-lg md:text-xl text-slate-50">
         Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-        cubilia curae; Proin sodales ultrices nulla blandit volutpat.
+        cubilia curae, Proin sodales ultrices nulla blandit volutpat.
       </p>
     </div>
   </section>
