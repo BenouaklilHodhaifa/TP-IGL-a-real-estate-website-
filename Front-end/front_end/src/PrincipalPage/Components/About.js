5158359d94a08c321c88759e6f47e56ac7edfd92
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2022-12-29 21:45:17 +0000

    some modifications in landing page

--- Front-end/front_end/src/PrincipalPage/Components/About.js
+++ Front-end/front_end/src/PrincipalPage/Components/About.js
@@ -9,15 +9,15 @@ const About = () => {
   ];
 
   return (
-    <section className="w-full">
-      <h1 className="w-full h-12 bg-slate-200 flex items-center justify-center text-bold text-4xl py-12 mb-4 font-serif">
+    <section className="w-full transition-all duration-500 ease-in" id="About">
+      <h1 className="w-full h-12 bg-slate-200 flex items-center justify-center text-bold text-4xl py-12 font-serif">
         About Us
       </h1>
-      <div className="w-full grid grid-cols-1 grid-rows-4 gap-3 md:grid-cols-2 md:grid-rows-2 items-center justify-items-center">
+      <div className="w-full grid grid-cols-1 grid-rows-4 gap-y-3 md:gap-x-1 sm:grid-cols-2 sm:grid-rows-2 items-center justify-items-center bg-slate-400 py-8">
         {Members.map((Member) => (
           <div
             key={Member.id}
-            className="w-3/4 md:w-2/3 h-40 my-3 border-4 rounded-xl"
+            className="w-3/4 sm:w-2/3 h-52 my-3 border-4 rounded-xl shadow-xl "
           >
             <h1>{Member.name}</h1>
           </div>
--- Front-end/front_end/src/PrincipalPage/Components/Header.js
+++ Front-end/front_end/src/PrincipalPage/Components/Header.js
@@ -1,43 +1,15 @@
 import React from "react";
 import "./Header.css";
-import { AiOutlineMail } from "react-icons/ai";
-import { BsFacebook, BsTelephone } from "react-icons/bs";
-import { AiFillLinkedin, AiOutlineHome } from "react-icons/ai";
-import { AiFillInstagram } from "react-icons/ai";
-import { FiMenu } from "react-icons/fi";
 import Navbar from "./Navbar";
 
 const Header = () => (
   <section className="h-[752px]" id="Hero">
-    <div className="w-full h-[80px] flex flex-row items-center justify-between px-8">
-      <div className="flex flex-row gap-2 max-w-[250px]">
-        <AiOutlineMail size={25} />
-        <h6>exemple@gmail.com</h6>
-      </div>
-      <div className="flex flex-row items-center justify-center gap-4 max-w-lg">
-        <div className="hidden md:flex md:flex-row gap-3 cursor-pointer">
-          <BsFacebook size={20} />
-          <AiFillLinkedin size={20} />
-          <AiFillInstagram size={20} />
-        </div>
-        <div className="flex flex-row items-center justify-center gap-1 border-l-[3px] pl-[6px]">
-          <BsTelephone size={15} />
-          <h5>123-234</h5>
-        </div>
-      </div>
-    </div>
-    <hr></hr>
-    <div className="w-full h-[60px] flex flex-row md:hidden items-center justify-center px-10 gap-8">
-      <BsFacebook size={30} />
-      <AiFillLinkedin size={30} />
-      <AiFillInstagram size={30} />
-    </div>
     <Navbar />
     <div className="w-full flex flex-col items-center justify-center mt-[170px] gap-4 px-3">
-      <h1 className="md:text-5xl text-3xl font-semibold text-slate-200">
+      <h1 className="md:text-5xl text-3xl font-semibold text-slate-400">
         Find your dream home
       </h1>
-      <p className="text-bold text-center text-lg md:text-xl text-slate-50">
+      <p className="text-bold text-center text-lg md:text-xl text-slate-500">
         Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
         cubilia curae, Proin sodales ultrices nulla blandit volutpat.
       </p>
