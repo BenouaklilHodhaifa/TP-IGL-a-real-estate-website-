9153a64b4d11333402268aa45f0a632c4cf11dbb
BenouaklilHodhaifa <kh_benouaklil@esi.dz>
2022-12-31 11:30:59 +0100

    added dirst and last name to users

--- Back-end/TP_IGL/main/models.py
+++ Back-end/TP_IGL/main/models.py
@@ -21,7 +21,8 @@ class UserAccountManager(BaseUserManager):
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     # name = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
+    first_name = models.CharField(
+        max_length=255, default="")  # i'll review this later
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
