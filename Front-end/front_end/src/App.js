94d4ba68353a608d212d5dcd5ee85e0ad6156940
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2023-01-04 21:24:36 +0000

    Create User Page Component

--- Front-end/front_end/src/App.js
+++ Front-end/front_end/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Contact from "./PrincipalPage/Components/Contact";
 import Header from "./PrincipalPage/Components/Header";
 import About from "./PrincipalPage/Components/About";
-import Ismail from "./PrincipalPage/Components/Ismail";
+
 function App() {
   return (
     <div>
---
+++ Front-end/front_end/src/UserPage/Components/Ai.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ai = () => {
+  return (
+    <div>
+      <h1>.;,ojiko</h1>
+    </div>
+  );
+};
+
+export default Ai;
