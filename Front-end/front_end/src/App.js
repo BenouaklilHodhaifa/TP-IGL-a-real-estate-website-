6745aff058e804403a9877beb62da6edcc8b9caf
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2023-01-06 21:22:15 +0000

    add user page components

--- Front-end/front_end/src/App.js
+++ Front-end/front_end/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Contact from "./PrincipalPage/Components/Contact";
 import Header from "./PrincipalPage/Components/Header";
 import About from "./PrincipalPage/Components/About";
-
+import Navigation from "./UserPage/Components/Navigation";
 function App() {
   return (
     <div>
---
+++ Front-end/front_end/src/UserPage/Components/Navigation.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { AiOutlineUser, AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const Navigation = () => {
+  let Links = [
+    { name: "Submit an ad", link: "/", id: 1 },
+    { name: "Search an ad", link: "/", id: 2 },
+    { name: "view posted ads", link: "/", id: 3 },
+    { name: "Messages", link: "/", id: 4 },
+  ];
+
+  let [Open, setOpen] = useState(false);
+
+  return (
+    <div className="shadow-md w-full sticky left-0  md:w-auto">
+      <div className="md:flex items-center justify-between py-4 md:px-4 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center justify-start font-[poppins] text-gray-800">
+          <div className="text-3xl text-indigo-300 mr-1 pt-2">
+            <AiOutlineUser size={40} />
+          </div>
+          <div className="pt-2">User name</div>
+        </div>
+        <div
+          onClick={() => setOpen(!Open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          {Open ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            Open ? "top-20" : "left-[-990px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.id} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-600 hover:border-b-4 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
