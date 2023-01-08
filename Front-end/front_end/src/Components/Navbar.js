aa58080aad9c4186e7b8eee074e962e20e927a0a
Hous1423 <kh_talbi@esi.dz>
2023-01-08 20:45:11 +0100

    front-end restructuration and additionnal modifications

--- Front-end/front_end/src/Components/Navbar.js
+++ Front-end/front_end/src/Components/Navbar.js
@@ -4,40 +4,37 @@ import Auth from "./Auth";
 
 const Navbar = () => {
   let Links = [
-    { name: "Home", link: "#Hero", id: 1 },
     { name: "About Us", link: "#About", id: 2 },
     { name: "Contact Us", link: "#Contact", id: 3 },
-    { name: "Home", link: "/", id: 4 },
-    { name: "Home", link: "/", id: 5 },
   ];
 
   let [Open, setOpen] = useState(false);
 
   return (
-    <div className="shadow-md w-full sticky left-0  md:w-auto">
+    <div className="shadow-md w-full sticky left-0  md:w-auto bg-[#2C3A61]">
       <div className="md:flex items-center justify-between py-4 md:px-4 px-7">
         <div className="font-bold text-2xl cursor-pointer flex items-center justify-start font-[poppins] text-gray-800">
-          <div className="text-3xl text-indigo-600 mr-1 pt-2">
+          <div className="text-3xl text-white mb-3 pt-2">
             <AiOutlineHome size={30} />
           </div>
-          <div className="pt-2">NAME</div>
+          <div className="pt-2 relative left-3 bottom-1 text-white">GOHOME</div>
         </div>
         <div
           onClick={() => setOpen(!Open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          className="text-white text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
         >
           {Open ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#2C3A61] md:bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             Open ? "top-20" : "left-[-990px]"
           }`}
         >
           {Links.map((link) => (
-            <li key={link.id} className="md:ml-8 text-xl md:my-0 my-7">
+            <li key={link.id} className=" md:ml-8 text-xl md:my-0 my-7">
               <a
                 href={link.link}
-                className="text-gray-600 hover:border-b-4 duration-500"
+                className="text-white hover:border-b-4 duration-500 "
               >
                 {link.name}
               </a>
@@ -53,3 +50,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
