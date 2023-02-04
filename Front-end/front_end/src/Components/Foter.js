8bd6768335d9e61fbd167bd0ef0510b91535e9c3
oussama <o.djelloul@esi-sba.dz>
2023-02-05 00:48:18 +0100

    Tp is done

--- Front-end/front_end/src/Components/Foter.js
+++ Front-end/front_end/src/Components/Foter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8">
+    <footer className="bg-gray-900 text-white py-8" data-testid='footer-1'>
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-between">
           <p className="text-sm">&copy; 2023 GOHOME</p>
